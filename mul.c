6a9d18546350c153e02319cd18687736eb1230cf
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-01 23:55:46 +0600

    Add files via upload

---
+++ mul.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    float a,b,multiplication;
+    a= 10;
+    b= 4;
+    multiplication= a * b;
+    printf("multiplication = %f\n",multiplication);
+    return 0;
+}
