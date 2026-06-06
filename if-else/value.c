3c835c0b1bf277e0a9ea25bde55214ca58cb24a6
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-06 19:41:18 +0600

    Add files via upload

---
+++ if-else/value.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter a number:\n");
+    scanf("%d",&a);
+    if(a<0){
+        printf("%d is a negative number.",a);
+    }
+    else if(a>0){
+        printf("%d is a positive number.",a);
+    }
+    else if(a==0){
+        printf("%d is zero.",a);
+    }
+return 0;
+}
