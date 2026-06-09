0e5a3ccdc49cfe4cd0d29db4bda108a48fb097d4
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-10 01:38:43 +0600

    Add files via upload

---
+++ loop/sum_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    int temp = n;
+    while(temp != 0) {
+        sum += temp % 10;
+        temp /= 10;
+    }
+    printf("Sum of digits = %d\n", sum);
+    return 0;
+}
