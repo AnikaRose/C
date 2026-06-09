f23aa5d6d09cb744f5e4d43d8c9acdda46292424
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-09 23:08:15 +0600

    Add files via upload

---
+++ loop/sum_n_even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n, i = 1, sum = 0;
+    printf("To find sum of all even numbers between 1 to n\n");
+    printf("Enter n:\n");
+    scanf("%d", &n);
+    while(i <= n) {
+        if(i % 2 == 0) sum += i;
+        i++;
+    }
+    printf("Sum of all even numbers between 1 to n is: %d", sum);
+    return 0;
+}
